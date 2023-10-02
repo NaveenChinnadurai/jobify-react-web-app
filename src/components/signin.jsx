2e0f609d990ea214e8162c6e4aeaf448c1b574bc
Naveen Chinnadurai <111662328+NaveenChinnadurai@users.noreply.github.com>
2023-10-02 09:37:53 +0530

    updated:font size and colorings

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,52 +1,55 @@
 import React from 'react'
 import '../styles/signin.css'
-import {FaGoogle as Google,FaLinkedin as Linked} from 'react-icons/fa'
-import {FaSquareFacebook as FB} from 'react-icons/fa6'
+import { FaGoogle as Google, FaLinkedin as Linked } from 'react-icons/fa'
+import { FaSquareFacebook as FB } from 'react-icons/fa6'
 function Signin() {
-  return (
-    <div className="col account-div">
-        <h2>Empower your career aspirations and unlock boundless opportunities <br /> Step into your future Now!</h2>
-        <div className='row signin-div' >
-            <div className="col signin">
-                <div className="col signin-title">
-                    <h2>Your dream job is just a login away!</h2>
-                    <p>One click away from the stars! Sign in and launch your soaring career path.</p>
-                </div>
-                <div className="signin-icons">
-                    <span><Google className='signin-icon'/></span>
-                    <span><FB className='signin-icon'/></span>
-                    <span><Linked className='signin-icon'/></span>
-                </div>
-                <div className="col signin-inputs">
-                    <input type="text" placeholder='Email/Mobile No.' className='signin-input'/>
-                    <input type="text" placeholder='Password' className='signin-input'/>
-                    <a href="#" className='pass'>Forget password</a>
-                    <span className='signin-btn'>Log In</span>
-                </div>
+    return (
+        <div className="col account-div">
+            <div className="sign-head">
+                <h2>Empower your career aspirations and unlock boundless opportunities.</h2>
+                <h2>Step into your future Now!</h2>
             </div>
-            <div className="col signup">
-                <div className="col signin-title">
-                    <h2>Join us and unlock a world of career possibilities</h2>
-                    <p>Ready to fuel your career growth? Sign up for an extraordinary journey!</p>
+            <div className='row signin-div' >
+                <div className="col signin">
+                    <div className="col signin-title">
+                        <h2>Your dream job is just a login away!</h2>
+                        <p>One click away from the stars! Sign in and launch your soaring career path.</p>
+                    </div>
+                    <div className="signin-icons">
+                        <span><Google className='signin-icon' /></span>
+                        <span><FB className='signin-icon' /></span>
+                        <span><Linked className='signin-icon' /></span>
+                    </div>
+                    <div className="col signin-inputs">
+                        <input type="text" placeholder='Email/Mobile No.' className='signin-input' />
+                        <input type="text" placeholder='Password' className='signin-input' />
+                        <a href="#" className='pass'>Forget password</a>
+                        <span className='signin-btn'>Log In</span>
+                    </div>
                 </div>
-                <p className='signup-head'>Sign up here </p>
-                <div className="row signup-inputs">
-                    <input type="text" placeholder='Email' className='signup-input'/>
-                    <input type="text" placeholder='Mobile No.' className='signup-input'/>
-                    <input type="text" placeholder='Create password' className='signup-input'/>
-                    <input type="text" placeholder='Confirm password' className='signup-input'/>
-                    <span className='signin-btn signup-btn'>Sign Up</span>
-                </div>
-                <p>OR</p>
-                <div className="signin-icons">
-                    <span><Google className='signin-icon'/></span>
-                    <span><FB className='signin-icon'/></span>
-                    <span><Linked className='signin-icon'/></span>
+                <div className="col signup">
+                    <div className="col signin-title">
+                        <h2>Join us and unlock a world of career possibilities</h2>
+                        <p>Ready to fuel your career growth? Sign up for an extraordinary journey!</p>
+                    </div>
+                    <p className='signup-head'>Sign up here </p>
+                    <div className="row signup-inputs">
+                        <input type="text" placeholder='Email' className='signup-input' />
+                        <input type="text" placeholder='Mobile No.' className='signup-input' />
+                        <input type="text" placeholder='Create password' className='signup-input' />
+                        <input type="text" placeholder='Confirm password' className='signup-input' />
+                        <span className='signin-btn signup-btn'>Sign Up</span>
+                    </div>
+                    <p>OR</p>
+                    <div className="signin-icons">
+                        <span><Google className='signin-icon' /></span>
+                        <span><FB className='signin-icon' /></span>
+                        <span><Linked className='signin-icon' /></span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Signin
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import {BsArrowUpCircle as Top} from 'react-icons/bs'
+import { BsArrowUpCircle as Top } from 'react-icons/bs'
 import '../styles/PagesStyles/home.css'
 import Partners from '../components/partners'
 import Banner from '../components/banner'
@@ -14,8 +14,6 @@ import Courses from '../components/courses'
 import JobFields from '../components/jobFields'
 
 function Home() {
-  const [showButton, setShowButton] = useState(false);
-
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -23,43 +21,32 @@ function Home() {
     });
   };
 
-  const handleScroll = () => {
-    if (window.scrollY > 100) {
-      setShowButton(true);
-    } else {
-      setShowButton(false);
-    }
-  };
-  window.addEventListener('scroll', handleScroll);
-
   return (
-    <div className=' '>
-        <Banner/>
-        <div className="quote-div col">
-            <h1>Fly like Flies</h1>
-            <h1>Grap Your Oppurtunies on Jobify</h1>
-            <h3>65% of the Jobify Aspirant Uncovered the right job prospects within 6 month from the day they become part of Jobify</h3>
-        </div>
-        <div className='search-div row'>
-            <span className='search-txt'>Explore Jobs</span>
-            <input type="text" placeholder='Browse Jobs here' className='search-tab'/>
-            <button   className='search-btn'>Search</button>
-        </div>
-        <div className="job-search col">
-          <h2>Explore the latest jobs</h2>
-          <JobFields />
-        </div>
-        <Partners/>
-        <About/>
-        <Features/>
-        <Missions/>
-        <Courses/>
-        <Highlights/>
-        <Signin/>
-        <AppPromo/>
-        {showButton && (
-          <button className="top-btn col" onClick={scrollToTop}><Top className='top-icon'/></button>
-      )}
+    <div className='home'>
+      <Banner />
+      <div className="quote-div col">
+        <h1>Fly like Flies</h1>
+        <h1>Grap Your Oppurtunies on Jobify</h1>
+        <h3>65% of the Jobify Aspirant Uncovered the right job prospects within 6 month from the day they become part of Jobify</h3>
+      </div>
+      <div className='search-div row'>
+        <span className='search-txt'>Explore Jobs</span>
+        <input type="text" placeholder='Browse Jobs here' className='search-tab' />
+        <button className='search-btn'>Search</button>
+      </div>
+      <div className="job-search col">
+        <h2>Explore the latest jobs</h2>
+        <JobFields />
+      </div>
+      <Partners />
+      <About />
+      <Features />
+      <Missions />
+      <Courses />
+      <Highlights />
+      <Signin />
+      <AppPromo />
+      <button className="top-btn col" onClick={scrollToTop}><Top className='top-icon' /></button>
     </div>
   )
 }
